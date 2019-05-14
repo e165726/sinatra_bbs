18efdfc7baee7a8bf4d1ba7fdf6d27e1b47362f8
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2019-05-14 22:04:12 +0900

    Add func delete

--- app.rb
+++ app.rb
@@ -3,19 +3,9 @@ require 'sinatra/reloader'
 require 'dotenv/load'
 require 'pg'
 
-# def connection
-#   begin
-#     return PG::connect(:dbname => 'bbs')
-#   rescue PG::Error => e
-#     puts e.message
-#   ensure
-#     PG::connect(:dbname => 'bbs').close
-#   end
-# end
-
 get '/' do
-  connection = PG::connect(:dbname => 'bbs')
-  @sql = connection.exec("SELECT * FROM boards")
+  db = PG::connect(:dbname => 'bbs')
+  @sql = db.exec("SELECT * FROM boards")
   erb :index
 end
 
@@ -23,9 +13,24 @@ post '/posts' do
   name = params[:name]
   email = params[:email]
   message = params[:message]
-  
-  connection = PG::connect(:dbname => 'bbs')
-  connection.exec("insert into boards (name, email, message) values($1, $2, $3)",[name, email, message])
+
+	unless name.empty? || email.empty? || message.empty?
+		db = PG::connect(:dbname => 'bbs')
+		db.exec("insert into boards (name, email, message) values($1, $2, $3)", [name, email, message])
+	end
   redirect "/"
 end
 
+get '/delete/:id' do
+	db = PG::connect(:dbname => 'bbs')
+  @item = db.exec("SELECT * FROM boards where $1", params[:id])
+	erb :delete
+end
+
+post '/delete/:id' do
+  if params.has_key?("ok")
+  	item = db.exec("SELECT * FROM boards where $1", params[:id])
+    item.destroy
+  end
+  redirect '/'
+end
